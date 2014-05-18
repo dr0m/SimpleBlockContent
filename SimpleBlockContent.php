aa25f97659fa16eb6e96c49feb85ec4ac7872e48
Luis Antonio <luis.agn@gmail.com>
2014-05-18 01:36:46 -0500

    Add support for I18n

--- SimpleBlockContent.php
+++ SimpleBlockContent.php
@@ -5,7 +5,7 @@ $thisfile = basename(__FILE__, ".php");
 
 # register plugin
 register_plugin(
-    $thisfile, 'SimpleBlockContent', '0.1.0', 'Luis Antonio', 'https://github.com/luis-agn', 'Replace place holders in pages with pages content', '', ''
+    $thisfile, 'SimpleBlockContent', '0.2.0', 'Luis Antonio', 'https://github.com/luis-agn', 'Replace place holders in pages with pages content', '', ''
 );
 
 # activate filter
@@ -20,15 +20,16 @@ function simple_block_content_match($match)
 {
     $id = $match['slug'];
     $file = 'data/pages/'.$id.'.xml';
-    if (file_exists($file))
-    {
-        $data_index = getXML($file);
-        $page = stripslashes(html_entity_decode($data_index->content, ENT_QUOTES, 'UTF-8'));
-    }
+
+    if (function_exists('return_i18n_page_data'))
+        $data = return_i18n_page_data($id);
+    elseif (file_exists($file))
+        $data = getXML($file);
     else
-    {
-        $page = "";
-    }
+        $page = '';
+
+    if (isset($data))
+        $page = stripslashes(html_entity_decode($data->content, ENT_QUOTES, 'UTF-8'));
 
     return $page;
 }
